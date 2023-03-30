5261eb6e3545f2ab4fe25729f740deae316f2b9e
Parkjunwu <phak9402@gmail.com>
2023-03-30 13:53:04 +0900

    second commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const newLine = () => {
+  console.log('Hello World');
+};
+
+const newLine2 = () => {
+  console.log('Hello World2');
+};
+
+const newLine3 = () => {
+  console.log('Hello World3');
+};
+
+const newLine4 = () => {
+  console.log('Hello World4');
+};
